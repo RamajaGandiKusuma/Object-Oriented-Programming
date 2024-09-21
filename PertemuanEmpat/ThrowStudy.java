3cd1b8fe572db3fb6cf76af8d2e04143a3ca8199
Ramaja Gandi Kusuma <ramajagandikusuma18@gmail.com>
2024-09-21 23:53:29 +0700

    Update ThrowStudy.java

--- PertemuanEmpat/ThrowStudy.java
+++ PertemuanEmpat/ThrowStudy.java
@@ -4,23 +4,41 @@
  */
 package PertemuanEmpat;
 
+import java.util.Scanner;
+
 /**
  *
  * @author RAMAJA
  */
 public class ThrowStudy {
-    
-    public int nilai(int a) throws ArithmeticException {
-        if(a<=75){
-            throw new ArithmeticException("Anda Tidak Lulus");
-        }else{
-            System.out.println("Anda Lulus");
-        }
-        return a;
+
+    public static void pilihMatrixs(int matriks [][], int baris, int kolom ) throws ArrayIndexOutOfBoundsException {
+           if(baris!=0||kolom!=0){
+            int nilai=matriks[baris-1][kolom-1];
+            System.out.println("Nilai: "+nilai);
+           }else{
+               throw new ArrayIndexOutOfBoundsException("BARIS DAN KOLOM YANG ANDA MASUKAN SALAH");
+           }
+            
     }
-    
-    public static void main(String[] args) {
-        ThrowStudy thr = new ThrowStudy();
-        thr.nilai(97);
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+          int matrx [][]={
+              {2, 4, 6},
+              {8, 10, 12},
+              {14, 16, 18}
+          };
+        
+        System.out.println("INI MATRIKS 3X3");
+        System.out.println("Pilih baris ke: ");
+        int a=input.nextInt();
+        System.out.println("Pilih kolom ke: ");
+        int b=input.nextInt();
+        pilihMatrixs(matrx,a,b);
+        
+        
+          
+          
     }
 }
